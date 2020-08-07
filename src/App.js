8058ad14e0044f2571018349081ec48d6db38554
satishgupta07 <satishgupta.cse01@gmail.com>
2020-08-07 10:31:07 +0530

    Set up React Router
    Signed-off-by: satishgupta07 <satishgupta.cse01@gmail.com>

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
 import React from 'react';
 import './App.css';
+import {BrowserRouter as Router, Switch, ROute, Link, Route } from 'react-router-dom'
 
 function App() {
   return (
-    <div className="App">
-       <h1>Let's Build an Amazon Clone with ReactJS</h1>
-    </div>
+    <Router>
+        <div className="App">
+           <Switch>
+             <Route path="/checkout">
+                <h1>Checkout</h1>
+             </Route>
+             <Route path="/login">
+                <h1>Login Pafge</h1>
+             </Route>
+             <Route path="/">
+                <h1>Home Page !!!</h1>
+             </Route>
+           </Switch>
+        </div>
+    </Router>
   );
 }
 
