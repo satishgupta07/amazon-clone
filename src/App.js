ab28892aa6986fc28e8c9b4707b2e27491db992c
satishgupta07 <satishgupta.cse01@gmail.com>
2020-08-10 17:45:54 +0530

    Working on Building NavBar
    Signed-off-by: satishgupta07 <satishgupta.cse01@gmail.com>

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './App.css';
-import {BrowserRouter as Router, Switch, ROute, Link, Route } from 'react-router-dom'
+import {BrowserRouter as Router, Switch, Link, Route } from 'react-router-dom'
+import Header from './Header';
 
 function App() {
   return (
@@ -11,9 +12,10 @@ function App() {
                 <h1>Checkout</h1>
              </Route>
              <Route path="/login">
-                <h1>Login Pafge</h1>
+                <h1>Login Page</h1>
              </Route>
              <Route path="/">
+                <Header />
                 <h1>Home Page !!!</h1>
              </Route>
            </Switch>
---
+++ src/Header.js
@@ -0,0 +1,33 @@
+import React from 'react'
+import "./Header.css"
+import { Link } from 'react-router-dom'
+import SearchIcon from '@material-ui/icons/Search';
+
+function Header() {
+    return (
+        <nav className="header">
+
+        {/* logo on the left -> img */}
+        <Link to="/">
+          <img 
+             className="header__logo" 
+             src="http://pngimg.com/uploads/amazon/amazon_PNG11.png"
+             alt=""
+           />
+        </Link>
+        
+        {/* Search Box */}
+         <div className="header__search">
+            <input type="text" className="header__searchInput" />
+            <SearchIcon className="header__searchIcon" />
+         </div>
+         
+
+        {/* 3 Links */}
+        {/* Basket Icon with number */}
+            
+        </nav>
+    )
+}
+
+export default Header
