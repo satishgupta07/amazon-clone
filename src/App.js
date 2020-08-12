ee604ba2f427e9d7f5ca32642ec8518785e82ebb
satishgupta07 <satishgupta.cse01@gmail.com>
2020-08-13 00:14:44 +0530

    Build the Banner
    Signed-off-by: satishgupta07 <satishgupta.cse01@gmail.com>

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './App.css';
 import {BrowserRouter as Router, Switch, Link, Route } from 'react-router-dom'
 import Header from './Header';
+import Home from './Home';
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
         <div className="App">
            <Switch>
              <Route path="/checkout">
+                <Header />
                 <h1>Checkout</h1>
              </Route>
              <Route path="/login">
@@ -16,28 +18,7 @@ function App() {
              </Route>
              <Route path="/">
                 <Header />
-                <h1>Home Page !!!</h1>
-                <h1>Home Page !!!</h1>
-                <h1>Home Page !!!</h1>
-                <h1>Home Page !!!</h1>
-                <h1>Home Page !!!</h1>
-                <h1>Home Page !!!</h1>
-                <h1>Home Page !!!</h1>
-                <h1>Home Page !!!</h1>
-                <h1>Home Page !!!</h1>
-                <h1>Home Page !!!</h1>
-                <h1>Home Page !!!</h1>
-                <h1>Home Page !!!</h1>
-                <h1>Home Page !!!</h1>
-                <h1>Home Page !!!</h1>
-                <h1>Home Page !!!</h1>
-                <h1>Home Page !!!</h1>
-                <h1>Home Page !!!</h1>
-                <h1>Home Page !!!</h1>
-                <h1>Home Page !!!</h1>
-                <h1>Home Page !!!</h1>
-                <h1>Home Page !!!</h1>
-                
+                <Home />
              </Route>
            </Switch>
         </div>
---
+++ src/Home.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import './Home.css'
+
+function Home() {
+    return (
+        <div className="home">
+            <img 
+                  className="home__image"
+                  src="https://ditsolutions.net/wp-content/uploads/2019/09/Jupiter_19_Banner_1._CB1198675309_.jpg"
+                  alt=""
+              />
+        </div>
+    )
+}
+
+export default Home
