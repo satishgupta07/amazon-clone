3e777d8764f9e69b968c1bc54b4fd3af433d24ae
satishgupta07 <satishgupta.cse01@gmail.com>
2020-08-14 01:31:35 +0530

    Building the Product Component
    Signed-off-by: satishgupta07 <satishgupta.cse01@gmail.com>

--- src/Home.js
+++ src/Home.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import './Home.css'
+import Product from './Product'
 
 function Home() {
     return (
@@ -9,6 +10,16 @@ function Home() {
                   src="https://ditsolutions.net/wp-content/uploads/2019/09/Jupiter_19_Banner_1._CB1198675309_.jpg"
                   alt=""
               />
+
+              {/* Product id, title, price, rating, image */}
+              <Product 
+                  id="12321341"
+                  title="Lorem Ipsum is simply dummy text of the printing and typesetting industry."
+                  price={11.56}
+                  rating={5}
+                  image="https://images-na.ssl-images-amazon.com/images/I/61zIwprkyhL._SX522_.jpg"
+              />
+              {/* Product */}
         </div>
     )
 }
